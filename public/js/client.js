8b0362f91d93be57511a9f577a6f6fd81ede2214
Rj <rj.bernaldo@gmail.com>
2014-08-10 16:43:56 -0700

    Fixed styling

--- public/js/client.js
+++ public/js/client.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
 	renderMainPage();
 	bindListeners();
+	window.scrollTo(0,1);
 });
 
 function bindListeners() {
+	var week = [
+		'Sunday',
+		'Monday',
+		'Tuesday',
+		'Thursday',
+		'Friday',
+		'Saturday'
+	]
 	$('#btn-home').on('click', function() {
 		renderMainPage();
 	});
@@ -13,13 +22,20 @@ function bindListeners() {
 			type: 'GET'
 		}).done(function(response) {
 			var items = $.parseJSON(response);
-			var ul = document.createElement('ul');
+			var ul = $('<ul id="history-list" data-autodividers="true" data-role="listview" data-filter="true"></ul>');
 			items.forEach(function(item) {
-				var li = document.createElement('li');
-				li.innerHTML = '<span>' + item.name + '</span>: <span>' + item.cost + '</span>';
-				ul.appendChild(li);
+				var day = week[new Date(item.createdAt).getDay()];
+				var li = $('<li day=' + day + '>' + item.name + ':' + item.cost + ':' + item.description + ':' + item.createdAt + '</li>');
+				ul.append(li);
 			})
-			$('#main-container').html(ul);
+			$('#main-container').html(ul).trigger('create');
+			$('#history-list').listview({
+				autodividers: true,
+				autodividersSelector: function(li) {
+					var out = li.attr('day');
+					return out;
+				}
+			}).listview('refresh');
 		})
 	});
 	$('#btn-statistics').on('click', function() {
@@ -28,39 +44,57 @@ function bindListeners() {
 	$('#btn-settings').on('click', function() {
 		$('#main-container').html('4');
 	});
-	$('#form-submit').on('submit', function() {
+	$('#btn-submit').on('click', function(e) {
+		e.preventDefault();
 		var name = $('#name').val();
 		var cost = $('#cost').val();
 		var description = $('#description').val();
-		$.ajax({
-			url: 'users/1/items/create',
-			type: 'POST',
-			dataType: 'json',
-			async: 'true',
-			data: {
-				name: name,
-				cost: cost,
-				description: description
-			},
-			success: function() {
-				$('#name').val("");
-				$('#cost').val("");
-				$('#description').val("");
-			}
-		});
+		if (name !== "" && cost !== "" && description !== "") {
+			$.ajax({
+				url: 'users/1/items/create',
+				type: 'POST',
+				dataType: 'json',
+				async: 'true',
+				data: {
+					name: name,
+					cost: cost,
+					description: description
+				},
+				success: function() {
+					$('#popup-confirm').html('<h3>New item created!</h3>' +
+																'<h4>' + name + '</h4>' +
+																'<p>' + cost + '</p>' +
+																'<p>' + description + '</p>');
+					$('#popup-confirm').popup("open");
+					setTimeout(function() {
+						$('#popup-confirm').popup("close");
+						$('#name').val("");
+						$('#cost').val("");
+						$('#description').val("");
+					}, 1500);
+				}
+			});
+		} else {
+			$('#popup-confirm').html('<h3>Please fill out all fields</h3>');
+			$('#popup-confirm').popup("open");
+			setTimeout(function() {
+				$('#popup-confirm').popup("close");
+			}, 1500);
+		}
 		return false;
 	});
 }
 
 function renderMainPage() {
 	$('#main-container').html('<div id="home-container"><form id="form-submit">' +
-		'<label for="name" class="ui-hidden-accessible">Name:</label>' +
+		'<label for="name" class="ui-hidden-accessible">Name</label>' +
 		'<input type="text" name="name" id="name" value="" placeholder="Name"/>' +
-		'<label for="cost" class="ui-hidden-accessible">Cost:</label>' +
+		'<label for="cost" class="ui-hidden-accessible">Cost</label>' +
 		'<input type="text" name="cost" id="cost" value="" placeholder="Cost"/>' +
-		'<label for="textarea-a" class="ui-hidden-accessible">Description:</label>' +
-		'<textarea name="textarea" id="description" placeholder="Description">' +
-		'</textarea>' +
+		'<label for="description" class="ui-hidden-accessible">Description</label>' +
+		'<input type="text" name="description" id="description" value="" placeholder="Description"/>' +
 		'<button id="btn-submit">Submit</button>' +
-	'</form></div>').trigger('create');
+	'</form></div>' +
+	'<div id="popup-confirm" data-role="popup" data-transition="fade" data-position-to="#home-container" data-shadow="false" data-overlay-theme="a" data-theme="none">' +
+	'</div>').trigger('create');
 }
